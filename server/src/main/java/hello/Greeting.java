ee26d4e56800eeb0224c907d1ced6de6ab272889
LLay <lay.lyon@gmail.com>
2016-05-23 18:54:42 -0400

    updated sample

--- server/src/main/java/hello/Greeting.java
+++ server/src/main/java/hello/Greeting.java
@@ -1,19 +1,20 @@
 package hello;
 
 public class Greeting {
-  private final long id;
-  private final String content;
 
-  public Greeting(long id, String content) {
-    this.id = id;
-    this.content = content;
-  }
+    private final long id;
+    private final String content;
 
-  public long getId() {
-    return id;
-  }
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
 
-  public String getContent() {
-    return content;
-  }
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
