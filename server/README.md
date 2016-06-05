7a72ac495c48e1b29e19356cdb6094860ae50b76
LLay <lay.lyon@gmail.com>
2016-06-05 19:23:58 -0400

    Created a configurable class to make scheduled requests

--- server/README.md
+++ server/README.md
@@ -1,4 +1,8 @@
-###build with 
-```./gradlew build```
-###run server on http://localhost:8080 with 
-```java -jar build/libs/gs-rest-service-0.1.0.jar```
+###build with
+```bash
+$ ./gradlew build
+```
+###run server on http://localhost:8080 with
+```bash
+$ java -jar build/libs/gs-rest-service-0.1.0.jar
+```
--- server/src/main/java/Application.java
+++ server/src/main/java/Application.java
@@ -7,5 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+
+    Poller poller = new Poller(1000, 10000, "http://api.eve-central.com/api/marketstat?typeid=34&typeid=35&regionlimit=10000002");
+    poller.poll();
   }
 }
