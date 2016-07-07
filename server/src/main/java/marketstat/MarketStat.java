f8fd9c72837ed36a61760873c1fd9b3250898f4e
Lyon Lay <llay@uncharted.software>
2016-07-07 10:30:26 -0400

    Renamed marketstat module

---
+++ server/src/main/java/marketstat/MarketStat.java
@@ -0,0 +1,13 @@
+package marketstat;
+
+public class MarketStat {
+  private final String content;
+
+  public MarketStat(String content) {
+    this.content = content;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
---
+++ server/src/main/java/marketstat/MarketStatController.java
@@ -0,0 +1,32 @@
+package marketstat;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+@RestController
+public class MarketStatController {
+
+  OkHttpClient client = new OkHttpClient();
+  public static String url = "http://api.eve-central.com/api/marketstat?typeid=34&typeid=35&regionlimit=10000002";
+
+  String run(String url) throws IOException {
+    Request request = new Request.Builder()
+    .url(url)
+    .build();
+
+    Response response = client.newCall(request).execute();
+    return response.body().string();
+  }
+
+  @RequestMapping("/marketstat")
+  public String marketstat() throws IOException {
+    return run(url);
+  }
+}
