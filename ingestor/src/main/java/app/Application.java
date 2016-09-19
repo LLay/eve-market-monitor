b4d6bda001b3e5488161c27f002037b66f82d4f4
Lyon Lay <llay@uncharted.software>
2016-09-19 10:21:03 -0400

    Attempted to get injestor running without springboot. failed

--- ingestor/src/main/java/app/Application.java
+++ ingestor/src/main/java/app/Application.java
@@ -5,11 +5,11 @@ import app.callback.*;
 public class Application {
   public static void main(String[] args) {
 
-    // Poller poller = new Poller(0, 10000, "http://api.eve-central.com/api/marketstat?typeid=34&typeid=35&regionlimit=10000002", new PrintResponseCallback());
-    Poller poller = new Poller(0,
-      10000,
-      "https://crest-tq.eveonline.com/market/prices/",
-      new WriteToRethinkCallback("eve", "marketprices", 28015, "admin"));
+    Poller poller = new Poller(0, 10000, "http://api.eve-central.com/api/marketstat?typeid=34&typeid=35&regionlimit=10000002", new PrintResponseCallback());
+    // Poller poller = new Poller(0,
+    //   10000,
+    //   "https://crest-tq.eveonline.com/market/prices/",
+    //   new WriteToRethinkCallback("eve", "marketprices", 28015, "admin"));
     poller.poll();
   }
 }
